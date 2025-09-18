52d7a8556310e356335846d5f12cace9471c969f
Lcocks <epx8hh@virginia.edu>
2025-09-18 05:09:28 +0000

    Created the bin/ dir, created the my_normalizer.py file and test ran.

---
+++ bin/my_normalizer.py
@@ -0,0 +1,69 @@
+##  Normalizer for ygainers.csv and wsjgainers.csv  ##
+
+import pandas as pd
+import numpy as np
+import re
+
+
+def normalize_yahoo(df):
+	"""
+	Normalizer for ygainers.csv
+	"""
+
+	# Update the column names
+	titles = ['symbol', 'company_name', 'Unnamed: 2','Price(extra)', 'change', 'perc_change', 'volume', '3_Month_Avg_Volume', 'Market Cap', 'P/E_Ratio (TTM)', '52_Wk_Change_%', '52_Wk_Range']
+	df.columns = titles
+	print(df[['Price(extra)']].head())
+
+	# Create the price column
+	df['price'] = df['Price(extra)'].apply(lambda x: str(x).split()[0])
+	print(df[['Price(extra)', 'price']].head())
+
+	# Drop the unnecessary columns and reorder
+	df.drop(['Unnamed: 2', 'Price(extra)', '3_Month_Avg_Volume', 'Market Cap', 'P/E_Ratio (TTM)', '52_Wk_Change_%', '52_Wk_Range'], axis=1, inplace=True)
+	df = df.iloc[:, [0, 1, 5, 2, 3, 4]]
+
+	# Update the perc_change and volume columns
+	df['perc_change'] = df['perc_change'].apply(lambda x: str(x).strip('+%'))
+	df['volume'] = df['volume'].apply(lambda x: str(round(float(str(x).strip('M')), 1)) + 'M')
+
+	# Handle missing values
+	numeric_cols = ['price', 'change', 'perc_change', 'volume']  # numeric columns
+	df[numeric_cols] = df[numeric_cols].replace(['nan', 'nanM'], None).fillna(np.nan) # convert to np.nan and infer type to handle deprecated behavior
+	df = df.dropna(subset=numeric_cols)
+
+
+	return df.to_csv('normalized_ygainers.csv', index=False)
+
+
+def normalize_wsj(df):
+	"""
+	Normalizer for wsjgainers.csv
+	"""
+
+	# Update the column names, set the regex and apply it
+	rex = r'\(([A-Z]+)\)$'
+	print(df.columns)
+	print(df[['Unnamed: 0']].head())
+	df.rename(columns={'Unnamed: 0': 'company_name', 'Volume': 'volume', 'Last': 'price', 'Chg': 'change', '% Chg': 'perc_change'}, inplace=True)
+	df['symbol'] = df['company_name'].apply(lambda x: re.findall(rex, x)[0])
+	df['company_name'] = df['company_name'].apply(lambda x: re.sub(rex, '', x))
+	print(df[['company_name', 'symbol']].head())
+
+	# Reorder
+	df = df.iloc[:, [5, 0, 2, 3, 4, 1]]
+
+	# Handle missing values
+	numeric_cols = ['price', 'change', 'perc_change', 'volume']  # your numeric columns
+	df[numeric_cols] = df[numeric_cols].replace(['nan', 'nanM'], None).fillna(np.nan) # convert to np.nan and infer type to handle deprecated behavior
+	df = df.dropna(subset=numeric_cols)
+
+
+	return df.to_csv('normalized_wsjgainers.csv', index=False)
+
+
+if __name__ == "__main__":
+
+	# Find and read the CSV files from anywhere in current directory tree
+	normalize_yahoo(pd.read_csv('../ygainers.csv', index_col=0))
+	normalize_wsj(pd.read_csv('../wsjgainers.csv', index_col=0))
